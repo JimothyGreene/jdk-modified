e7dc80196dca103274e4fc6122504f2474b791df
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2022-11-03 14:24:08 -0500

    basic iff logic working based on ternary

--- src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -936,7 +936,7 @@ public class JavacParser implements Parser {
      */
     JCExpression term1() {
         JCExpression t = term2();
-        if ((mode & EXPR) != 0 && token.kind == QUES) {
+        if ((mode & EXPR) != 0 && (token.kind == QUES || token.kind == IFF)) {
             selectExprMode();
             return term1Rest(t);
         } else {
@@ -954,6 +954,14 @@ public class JavacParser implements Parser {
             accept(COLON);
             JCExpression t2 = term1();
             return F.at(pos).Conditional(t, t1, t2);
+        } else if (token.kind == IFF) {
+            System.out.println("IFF");
+            int pos = token.pos;
+            nextToken();
+            JCExpression t1 = term();
+            accept(ELSE);
+            JCExpression t2 = term1();
+            return F.at(pos).Conditional(t1, t, t2);
         } else {
             return t;
         }
---
+++ tests/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String... args) {
+        System.out.println("Hello world");
+        int a = true ? 6 : 3;
+        int b = 6 iff false else 3;
+        System.out.println(b);
+    }
+}
